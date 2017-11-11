8c45dd5acbaa5697eb28fa410a658977b2faf5e4
Shivang Bhandari <pump382156@gmail.com>
2017-11-11 15:43:37 +0530

    Morse Conversion function added

--- swing.py
+++ swing.py
@@ -53,3 +53,16 @@ key = {
 }
 
 reverseKey = {b:a for a,b in key.items()}
+
+def doMorse(string):
+    encoded=''
+    for letter in string:
+        if (letter==' '):
+            encoded+=' '
+        if (letter.upper() in key):
+            letter=letter.upper()
+            encoded+=(key[str(letter)])+' '
+        else:
+            error="Invalid Character '"+letter+"' "
+            return error
+    return encoded
